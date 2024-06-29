6134355bce65107f058f47e5d179a483f8ff4f75
Mariam <mari.kifshidze@gmail.com>
2024-06-29 12:44:36 +0400

    Error handling

--- blog_post.py
+++ blog_post.py
@@ -12,3 +12,14 @@ def index():
 @app.route('/user/<int:user_id>')
 def user(user_id):
     return render_template('user.html', user_id=user_id)
+
+
+# Create custom error pages
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 400
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
