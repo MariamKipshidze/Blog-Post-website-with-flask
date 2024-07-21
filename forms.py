099ac81d21a688c62341f70f983a9e07f782d678
Mariam <mari.kifshidze@gmail.com>
2024-07-21 14:29:57 +0400

    Create UserForm

--- forms.py
+++ forms.py
@@ -6,3 +6,9 @@ from wtforms.validators import DataRequired, Email
 class EmailForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
+
+
+class UserForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
