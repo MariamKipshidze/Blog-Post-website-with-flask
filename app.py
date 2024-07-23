6539913466a0427c3fc9b8988c1bc8e1efc25ef8
Mariam <mari.kifshidze@gmail.com>
2024-07-23 15:36:49 +0400

    Add mysql database

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 def get_app():
     with app.app_context():
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+        # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://myuser:1@localhost/my_new_database'
         app.config['SECRET_KEY'] = 'secret'
     return app
